b1141e1425eca465bf162320217229fd0f427b33
thanh <thanh@7b5bf505-3498-49f7-8177-37ec8b90719f>
2022-05-23 06:06:56 +0000

    change error to warning when detected unsupported version of OS/2 table in ttf fonts
    git-svn-id: svn://tug.org/pdftex/branches/stable@882 7b5bf505-3498-49f7-8177-37ec8b90719f

--- source/src/texk/web2c/pdftexdir/writettf.c
+++ source/src/texk/web2c/pdftexdir/writettf.c
@@ -1217,7 +1217,7 @@ static void ttf_write_OS2(void)
     ttf_reset_chksm(tab);
     version = get_ushort();
     if (version > 5)
-        pdftex_fail("unknown version of OS/2 table (%.4X)", version);
+        pdftex_warn("unknown version of OS/2 table (%.4X)", version);
     (void) put_ushort(0x0001);  /* fix version to 1 */
     ttf_ncopy(2 * TTF_USHORT_SIZE + 13 * TTF_SHORT_SIZE + 10 * TTF_BYTE_SIZE);
     ttf_skip(4 * TTF_ULONG_SIZE);       /* ulUnicodeRange 1--4 */
